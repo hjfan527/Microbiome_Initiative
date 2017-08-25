29b1ec5c8dde3ab2e5790da76c1f6f75b7edf5b5
kdileep1994 <k.dileep1994@gmail.com>
2017-08-25 12:03:03 -0400

    some js updates

--- static/js/scripts/carousel.js
+++ static/js/scripts/carousel.js
@@ -1,1 +1,10 @@
 $('.carousel.carousel-slider').carousel({ fullWidth: true });
+
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
+
+$(function() {
+    autoplay();
+})
